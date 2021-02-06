691a11823df6865b32e6a3aaebf3967b20bc0a3f
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-06 02:16:19 +0000

    added files

---
+++ script.js
@@ -0,0 +1,4 @@
+// Selecting page elements
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+const countryInput = document.querySelector('#countrySearch');
